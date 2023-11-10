a9e462da19a85b3adfbeafba9fd8a3168769b3fc
Pratik Mhatre <146018586+mhatre9602@users.noreply.github.com>
2023-11-10 16:58:13 +0530

    Add files via upload

--- util/database.js
+++ util/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const pg = require("pg");
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -6,6 +7,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     dialect: "postgres",
+    dialectModule: pg,
     host: process.env.DB_HOSTNAME,
     port: process.env.DB_PORT,
   }
